181988e56ee08b1b0164606e0daddafc3d49f03e
sakis-anast <sak.anastasopoulos@gmail.com>
2023-02-03 13:37:47 +0200

    deployment

--- server/server.js
+++ server/server.js
@@ -22,8 +22,9 @@ app.listen(port, () => {
   console.log(`server is connected on port : ${port} `);
 });
 if (process.env.NODE_ENV === 'production') {
-  //*Set static folder
-  app.use(express.static('client/build'));
-  
-  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  app.use(express.static('../client/dist'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('../client', 'dist', 'index.html'));
+  });
 }
